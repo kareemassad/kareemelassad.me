7abe0d738853ab3436dc4aa550459582d5af1da2
kareemassad <kareemassad5@gmail.com>
2023-10-16 19:04:06 -0400

    init

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,28 @@
+import React from "react";
+import './Footer.scss';
+
+const Footer = () => {
+    return (
+        <footer className="footer-wrapper">
+            <div className="container justify-content-center">
+                <div className="say-hello">
+                    <h1>Say hello on any of these platforms!</h1>
+                </div>
+                <div className="row social">
+                    <a className="no-underline icon-link" href="https://www.linkedin.com/in/kareem-el-assad-759113140/" aria-label="LinkedIn">
+                        <i className="fab fa-linkedin"></i>
+                    </a>
+                    <a className="no-underline icon-link" href="https://github.com/kareemassad" aria-label="Github">
+                        <i className="fab fa-github"></i>
+                    </a>
+                    <a className="no-underline icon-link" href="mailto:kareemassad5@gmail.com" aria-label="Email">
+                        <i className="fas fa-envelope"></i>
+                    </a>
+                </div>
+            </div>
+            <div className="bottom-line"></div>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import './Header.scss';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Kareem El Assad</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link href="#projects">Projects</Nav.Link>
+                            <Nav.Link href="#involvement">Involvement</Nav.Link>
+                            <Nav.Link href="#resume">Resume</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    );
+};
+
+export default Header;
