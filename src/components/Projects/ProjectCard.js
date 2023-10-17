7328d55e66bf4bc976b17bf31d81e42dabd019af
kareemassad <kareemassad5@gmail.com>
2023-10-17 19:02:26 -0400

    adding projects, fixed cards, added hackathon projects, added src code, added imgs, added packages, added gh-pages

--- src/components/Projects/ProjectCard.js
+++ src/components/Projects/ProjectCard.js
@@ -6,7 +6,9 @@ import './ProjectCard.scss';
 const ProjectCard = ({ image, title, link }) => { // Changed to individual props
     return (
         <div className="project-card">
-            <img src={image} alt={title} className="project-image" />
+            <div className="project-image-wrapper">
+                <img src={image} alt={title} className="project-image" />
+            </div>
             <div className="project-content">
                 <h3 className="project-title">{title}</h3>
                 <a href={link} target="_blank" rel="noopener noreferrer" className="project-link">View Project</a>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,10 +5,18 @@ import ProjectCard from './ProjectCard';
 
 
 //imgs
+import basil_img from '../../assets/images/basil-pic.png';
 import autonomousImg from '../../assets/images/autonomous.png';
 import sudokuGif from '../../assets/gifs/full-gif-99comp.gif';
 import allauthlogo from '../../assets/images/allauth.png';
 import jukeboxImg from '../../assets/images/jukebox.jpg';
+import conuhacksImg from '../../assets/images/Arbitrading.png';
+import jsimg from '../../assets/images/JavaScript-logo.png';
+import empowerImg from '../../assets/images/empower.png';
+import hevyImg from '../../assets/images/hevyimg.png';
+import cuHackingimg from '../../assets/images/Button_go.gif';
+import manhuntimg from '../../assets/images/portable-saw.jpg';
+import cuimg from '../../assets/images/cu.png';
 
 const projectList = [
     {
@@ -36,35 +44,75 @@ const projectList = [
         displayType: 'detailed'
     },
     {
-        title: 'NFC Jukebox',
+        title: 'Carleton Software Engineering Repository',
         type: 'Personal Project',
-        description: 'Used Python to interface with an MFRC522 microcontroller to read NFC tags and play music based on the tag that was scanned. The application was hosted on a Raspberry Pi and used the Spotify API to play music on Amazon Alexa.',
-        image: "blank",
-        link: 'https://github.com/kareemassad/NFC-jukebox/',
+        description: 'A repository that contains all the assignments and projects that I completed during my time at Carleton University. The repository contains projects that were built using Java, C, C++, Python, PHP, and JavaScript.',
+        image: cuimg,
+        link: 'https://github.com/kareemassad/carleton_software_engineering',
         displayType: 'card'
     },
     {
         title: 'NFC Jukebox',
         type: 'Personal Project',
         description: 'Used Python to interface with an MFRC522 microcontroller to read NFC tags and play music based on the tag that was scanned. The application was hosted on a Raspberry Pi and used the Spotify API to play music on Amazon Alexa.',
-        image: "blank",
+        image: jukeboxImg,
         link: 'https://github.com/kareemassad/NFC-jukebox/',
         displayType: 'card'
     },
     {
-        title: 'NFC Jukebox',
-        type: 'Personal Project',
-        description: 'Used Python to interface with an MFRC522 microcontroller to read NFC tags and play music based on the tag that was scanned. The application was hosted on a Raspberry Pi and used the Spotify API to play music on Amazon Alexa.',
-        image: "blank",
-        link: 'https://github.com/kareemassad/NFC-jukebox/',
+        title: 'ConUHacksVII: Arbitrading',
+        type: 'Hackathon Project',
+        description: 'Used Python, Flask, React, and MongoDB Atlas to process, analyze, and display a large scale of data from the stock market.',
+        image: conuhacksImg,
+        link: 'https://github.com/kareemassad/ConUHacksVII',
         displayType: 'card'
     },
     {
-        title: 'NFC Jukebox',
+        title: '30 Days of JavaScript Challenge',
+        type: 'Challenge',
+        description: 'Completed the 30 Days of JavaScript challenge by Wes Bos. The challenge consisted of 30 mini projects that were built using JavaScript.',
+        image: jsimg,
+        link: 'https://github.com/kareemassad/30-days-of-javascript',
+        displayType: 'card'
+    },
+    {
+        title: 'HackWestern 6: Empower',
+        type: 'Hackathon Project',
+        description: 'Empower is a web application that allows users to identify and post about local and global protests. The project was built using Python, Flask, MongoDB Atlas, and the Google Maps API.',
+        image: empowerImg,
+        link: 'https://github.com/kareemassad/Empower',
+        displayType: 'card'
+    },
+    {
+        title: 'uOttaHack 3: Basil',
+        type: 'Hackathon Project',
+        description: 'Basil takes ingredients you already have and suggests healthy recipes you can make. The project was built using JavaScript, React, Firebase.',
+        image: basil_img,
+        link: 'https://github.com/kareemassad/basil',
+        displayType: 'card'
+    },
+    {
+        title: 'HevyAPI',
         type: 'Personal Project',
-        description: 'Used Python to interface with an MFRC522 microcontroller to read NFC tags and play music based on the tag that was scanned. The application was hosted on a Raspberry Pi and used the Spotify API to play music on Amazon Alexa.',
-        image: "blank",
-        link: 'https://github.com/kareemassad/NFC-jukebox/',
+        description: 'Created an unoffical API using Python and Flask to store and retrieve data for the Hevy Gym application.',
+        image: hevyImg,
+        link: 'https://github.com/kareemassad/hevyAPI/tree/main',
+        displayType: 'card'
+    },
+    {
+        title: 'CuHacking 2019: Assignment Sorter',
+        type: 'Hackathon Project',
+        description: 'The program prompts the user with a GUI which allows them to input several keywords. Those keywords are then used to parse and sort a set of files assigned by the user.',
+        image: cuHackingimg,
+        link: 'https://github.com/kareemassad/assignment-sorter',
+        displayType: 'card'
+    },
+    {
+        title: 'Local Hack Day 2019: 4-player Manhunt Game',
+        type: 'Hackathon Project',
+        description: 'Inspired originally by the game mechanics of Pacman and the logic of the game manhunt, this game is a 4-player game that involves the hunters chasing prey until one team wins against the other.',
+        image: manhuntimg,
+        link: 'https://github.com/kareemassad/hevyAPI/tree/main',
         displayType: 'card'
     }
 ]
