e09043c2e27d882fc3f49823a363c4a24704bc18
kareemassad <kareemassad5@gmail.com>
2023-11-03 15:44:28 -0400

    Adding small changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import logo from './logo.svg';
 import './App.scss';
-import "@vetixy/circular-std"; //font
 import Header from './components/Header/Header';
 import Projects from './components/Projects/Projects';
 import Involvement from './components/Involvement/Involvement';
